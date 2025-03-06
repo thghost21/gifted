aac2a061a09264f9124b86eeddd02c2decd28199
thghost21 <tylerhacker21@gmail.com>
2025-03-06 16:11:49 -0700

    tags and fixing columns

--- app/models/Gift.js
+++ app/models/Gift.js
@@ -3,13 +3,18 @@ export class Gift {
     this.url = data.url
     this.id = data.id
     this.opened = data.opened
+    this.tag = data.tag
 
   }
 
   get giftLayout() {
     return `
-    <div class="col-2">
-      <img  type="button" onclick="app.giftsController.openGift('${this.id}')" src="${this.url}"alt="gift"></div>
+    <div class="col-6 mb-3">
+      <div class="card"> 
+      <img class="img-fluid" type="button" onclick="app.giftsController.openGift('${this.id}')" src="${this.url}"alt="gift">
+      <div class="card-body">${this.tag}</div>
+      </div>
+      </div>
     `
 
   }
