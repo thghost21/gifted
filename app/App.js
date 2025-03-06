a69b978b1e9387f70f52a598a54438dcc0379b54
thghost21 <tylerhacker21@gmail.com>
2025-03-06 14:13:34 -0700

    login

--- app/App.js
+++ app/App.js
@@ -1,7 +1,8 @@
+import { AuthController } from "./Auth/AuthController.js"
 import { GiftsController } from "./controllers/GiftsController.js"
 
 class App {
-
+  authController = new AuthController()
   giftsController = new GiftsController()
 
 }
--- app/AppState.js
+++ app/AppState.js
@@ -1,9 +1,12 @@
+import { Identity } from './Auth/Identity.js'
 import { Gift } from './models/Gift.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
+  /** @type {Identity}*/
+  identity = null
   /** @type {Gift[]}*/
   gifts = []
 }
